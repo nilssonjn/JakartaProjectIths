5f36237d00e82641f8eb88ed491c4cf4e1788dcf
jnnilsson <jn.jensnilsson@gmail.com>
2024-02-22 12:33:34 +0100

    add entity customer.java

---
+++ src/main/java/com/example/jakartaeeiths/entity/Customer.java
@@ -0,0 +1,71 @@
+package com.example.jakartaeeiths.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "Customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customerId", nullable = false)
+    private Integer id;
+
+    @Size(max = 50)
+    @Column(name = "customerFirstName", length = 50)
+    private String customerFirstName;
+
+    @Size(max = 50)
+    @Column(name = "customerSurname", length = 50)
+    private String customerSurname;
+
+    @Column(name = "customerAge")
+    private Integer customerAge;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerSurname() {
+        return customerSurname;
+    }
+
+    public void setCustomerSurname(String customerSurname) {
+        this.customerSurname = customerSurname;
+    }
+
+    public Integer getCustomerAge() {
+        return customerAge;
+    }
+
+    public void setCustomerAge(Integer customerAge) {
+        this.customerAge = customerAge;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return getId() != null && Objects.equals(getId(), customer.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
+}
