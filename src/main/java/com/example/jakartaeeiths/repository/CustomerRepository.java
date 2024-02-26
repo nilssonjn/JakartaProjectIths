2ea6728b46123a264030f2dbd2a16979691a65f1
jnnilsson <jn.jensnilsson@gmail.com>
2024-02-26 10:01:01 +0100

    setup for connecting to mysql

--- src/main/java/com/example/jakartaeeiths/repository/CustomerRepository.java
+++ src/main/java/com/example/jakartaeeiths/repository/CustomerRepository.java
@@ -1,4 +1,16 @@
 package com.example.jakartaeeiths.repository;
 
-public class CustomerRepository {
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.io.Serializable;
+
+@ApplicationScoped
+public class CustomerRepository implements Serializable {
+
+    @PersistenceContext(unitName = "mysql")
+    EntityManager entityManager;
+
+    // query + @Transactional methods
 }
