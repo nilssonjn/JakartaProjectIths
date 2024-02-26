c60903b9829f448249ec8e341157b8c2ce400371
jnnilsson <jn.jensnilsson@gmail.com>
2024-02-26 12:54:06 +0100

    add get all

--- src/main/java/com/example/jakartaeeiths/repository/CustomerRepository.java
+++ src/main/java/com/example/jakartaeeiths/repository/CustomerRepository.java
@@ -1,16 +1,24 @@
 package com.example.jakartaeeiths.repository;
 
+import com.example.jakartaeeiths.entity.Customer;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
 import java.io.Serializable;
+import java.util.List;
 
 @ApplicationScoped
 public class CustomerRepository implements Serializable {
 
     @PersistenceContext(unitName = "mysql")
     EntityManager entityManager;
+
+    public List<Customer> getAll() {
+        return entityManager
+                .createQuery("select c from Customer c", Customer.class)
+                .getResultList();
+    }
 
     // query + @Transactional methods
 }
--- src/main/java/com/example/jakartaeeiths/resource/CustomerResource.java
+++ src/main/java/com/example/jakartaeeiths/resource/CustomerResource.java
@@ -1,6 +1,10 @@
 package com.example.jakartaeeiths.resource;
 
+import com.example.jakartaeeiths.entity.Customer;
+import com.example.jakartaeeiths.repository.CustomerRepository;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -11,7 +15,13 @@ import java.util.List;
 @Path("/hello")
 public class CustomerResource {
 
-//    @GET
+    public CustomerResource() {
+    }
+
+    @Inject
+    CustomerRepository customerRepository;
+
+    //    @GET
 //    @Produces("text/plain")
 //    public String hello() {
 //        return "Hello, World!";
@@ -22,5 +32,11 @@ public class CustomerResource {
     public Response sayHello() {
         String message = "Hello!";
         return Response.ok(message).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Customer> getAll() {
+        return customerRepository.getAll();
     }
 }
